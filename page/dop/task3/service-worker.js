49122163131d558328e50f3c771a58ac83ed0cf4
Asdmitruk <dmitrukadrian@gmail.com>
2023-09-26 03:03:53 +0300

    add_dop

---
+++ page/dop/task3/service-worker.js
@@ -0,0 +1,29 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("my-cache").then((cache) => {
+      return cache.addAll(["./task3.html", "./img/content.jpg"]);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== "my-cache") {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
